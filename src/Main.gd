367cf995b5839b1bbf8606f33599bf8c50f5853c
Dan Isamu <danilo.inafuku@dcomp.sor.ufscar.br>
2022-07-16 14:10:41 -0300

    color swapping

--- src/Main.gd
+++ src/Main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 onready var ui = $CanvasLayer/UI
+onready var shaders = $CanvasLayer/ScreenShaders
 onready var player = $YSort/Player
 onready var camera = $YSort/Player/Camera
 
@@ -12,13 +13,14 @@ func _enter_tree():
 
 
 func _on_Player_rolled(_direction):
-	var faces = player.dieFaces
+	var faces : DieFaces = player.dieFaces
 	ui.set_current_faces(faces.get_face(Constants.TOP), 
 		faces.get_face(Constants.BEHIND),
 		faces.get_face(Constants.FRONT), 
 		faces.get_face(Constants.LEFT), 
 		faces.get_face(Constants.RIGHT),
 		faces.get_face(Constants.DOWN))
+	shaders.set_current_color(faces.get_current_enemy_type())
 	
 	camera.shake_camera(80)
 
--- src/entities/player/DieFace.gd
+++ src/entities/player/DieFace.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name DieFaces
 
 const FACE_ENEMIES = [Constants.MELEE, Constants.RANGED, Constants.SUPPORT,
 					Constants.MELEE, Constants.RANGED, Constants.SUPPORT]
