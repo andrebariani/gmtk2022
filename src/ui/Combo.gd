196fa7419fb86f4062ce58dd715c3153f6d2d8ed
Dan Isamu <danilo.inafuku@dcomp.sor.ufscar.br>
2022-07-16 22:26:50 -0300

    rework da ui

--- src/ui/Combo.gd
+++ src/ui/Combo.gd
@@ -17,9 +17,9 @@ func set_next_combo_face(next_face_texture):
 func trigger_combo():
 	back.visible = false
 	tween.interpolate_property(popper, "rect_position", 
-		Vector2(0, 0), Vector2(305, 0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN, 0.1)
+		Vector2(0, 0), Vector2(280, 0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN, 0.1)
 	tween.interpolate_property(popper, "rect_position", 
-		Vector2(305, 0), Vector2(0, 0), 0.25, Tween.TRANS_BACK, Tween.EASE_IN, 0.45)
+		Vector2(280, 0), Vector2(0, 0), 0.25, Tween.TRANS_BACK, Tween.EASE_IN, 0.45)
 	
 	tween.interpolate_property(popper, "rect_rotation", 
 		0, 360, 0.4, Tween.TRANS_BACK)
